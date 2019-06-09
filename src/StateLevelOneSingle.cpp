204047283950a35793f6c2fe88f2687a0f968116
Luca Zenobi <luzenobi@gmail.com>
2019-06-09 16:10:10 +0200

    LevelOne: changed front texture

--- src/StateLevelOneSingle.cpp
+++ src/StateLevelOneSingle.cpp
@@ -15,18 +15,25 @@ namespace re {
 		sf::Texture& tex3 = engine.getTextureAssets()->loadTexture("TEX_CARD_3");
 		sf::Texture& tex4 = engine.getTextureAssets()->loadTexture("TEX_CARD_4");
 
-		gameObjects[0] = make_unique<CardObject>(CardObject("CARD_1", engine, tex1, tex1, 1));
-		gameObjects[1] = make_unique<CardObject>(CardObject("CARD_2", engine, tex2, tex2, 1));
-		gameObjects[2] = make_unique<CardObject>(CardObject("CARD_3", engine, tex3, tex3, 2));
-		gameObjects[3] = make_unique<CardObject>(CardObject("CARD_4", engine, tex4, tex4, 2));
-		gameObjects[4] = make_unique<CardObject>(CardObject("CARD_5", engine, tex1, tex1, 3));
-		gameObjects[5] = make_unique<CardObject>(CardObject("CARD_6", engine, tex2, tex2, 3));
-		gameObjects[6] = make_unique<CardObject>(CardObject("CARD_7", engine, tex3, tex3, 4));
-		gameObjects[7] = make_unique<CardObject>(CardObject("CARD_8", engine, tex4, tex4, 4));
-		gameObjects[8] = make_unique<CardObject>(CardObject("CARD_9", engine, tex1, tex1, 5));
-		gameObjects[9] = make_unique<CardObject>(CardObject("CARD_10", engine, tex2, tex2, 5));
-		gameObjects[10] = make_unique<CardObject>(CardObject("CARD_11", engine, tex3, tex3, 6));
-		gameObjects[11] = make_unique<CardObject>(CardObject("CARD_12", engine, tex4, tex4, 6));
+		sf::Texture& texFront1 = engine.getTextureAssets()->loadTexture("TEX_CARD_FRONT_1");
+		sf::Texture& texFront2 = engine.getTextureAssets()->loadTexture("TEX_CARD_FRONT_2");
+		sf::Texture& texFront3 = engine.getTextureAssets()->loadTexture("TEX_CARD_FRONT_3");
+		sf::Texture& texFront4 = engine.getTextureAssets()->loadTexture("TEX_CARD_FRONT_4");
+		sf::Texture& texFront5 = engine.getTextureAssets()->loadTexture("TEX_CARD_FRONT_5");
+		sf::Texture& texFront6 = engine.getTextureAssets()->loadTexture("TEX_CARD_FRONT_6");
+
+		gameObjects[0] = make_unique<CardObject>(CardObject("CARD_1", engine, texFront1, tex1, 1));
+		gameObjects[1] = make_unique<CardObject>(CardObject("CARD_2", engine, texFront1, tex2, 1));
+		gameObjects[2] = make_unique<CardObject>(CardObject("CARD_3", engine, texFront2, tex3, 2));
+		gameObjects[3] = make_unique<CardObject>(CardObject("CARD_4", engine, texFront2, tex4, 2));
+		gameObjects[4] = make_unique<CardObject>(CardObject("CARD_5", engine, texFront3, tex1, 3));
+		gameObjects[5] = make_unique<CardObject>(CardObject("CARD_6", engine, texFront3, tex2, 3));
+		gameObjects[6] = make_unique<CardObject>(CardObject("CARD_7", engine, texFront4, tex3, 4));
+		gameObjects[7] = make_unique<CardObject>(CardObject("CARD_8", engine, texFront4, tex4, 4));
+		gameObjects[8] = make_unique<CardObject>(CardObject("CARD_9", engine, texFront5, tex1, 5));
+		gameObjects[9] = make_unique<CardObject>(CardObject("CARD_10", engine, texFront5, tex2, 5));
+		gameObjects[10] = make_unique<CardObject>(CardObject("CARD_11", engine, texFront6, tex3, 6));
+		gameObjects[11] = make_unique<CardObject>(CardObject("CARD_12", engine, texFront6, tex4, 6));
 
 		const float x_OFFSET = 282.f;
 		const float Y_OFFSET = 360.f;
--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,13 @@ int main() {
 	engine->registerTexture("TEX_CARD_3", "resources/card_lunar_new_year_150_220.png");
 	engine->registerTexture("TEX_CARD_4", "resources/card_maraad_150_220.png");
 
+	engine->registerTexture("TEX_CARD_FRONT_1", "resources/arthas_150_220.png");
+	engine->registerTexture("TEX_CARD_FRONT_2", "resources/jaina_150_220.png");
+	engine->registerTexture("TEX_CARD_FRONT_3", "resources/medivh_150_220.png");
+	engine->registerTexture("TEX_CARD_FRONT_4", "resources/sylvanas_150_220.png");
+	engine->registerTexture("TEX_CARD_FRONT_5", "resources/thrall_150_220.png");
+	engine->registerTexture("TEX_CARD_FRONT_6", "resources/troll_150_220.png");
+
 	engine->registerState("SPLASH_SCREEN", make_shared<StateSplashScreen>(StateSplashScreen(*engine)));
 	engine->registerState("MAIN_MENU", make_shared<StateMainMenu>(StateMainMenu(*engine)));
 	engine->registerState("LEVEL_ONE_SINGLE", make_shared<StateLevelOneSingle>(StateLevelOneSingle(*engine)));
